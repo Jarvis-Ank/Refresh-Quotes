764e23d8a06e31ab8616d2b04c30c2473292b3a9
Aswin N Kaimal <aswinkaimal@gmail.com>
2022-11-14 20:02:49 +0530

    Upload files..

---
+++ pages/Components/Quote.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import styles from "./Quote.module.css";
+const Quote = () => {
+    const [randomQuote, setRandomQuote] = useState([]);
+    const [msg, setMessage] = useState("");
+    const rfqURL = "https://jarvis-ank.github.io/Refresh-Quotes/";
+
+    const url = "https://type.fit/api/quotes";
+    useEffect(() => {
+        getQuote();
+    }, []);
+    useEffect(() => {
+        var str = '"' + randomQuote.text + '"' + ' --' + randomQuote.author + '';
+        console.log(typeof (str) + str)
+        setMessage(str)
+    }, [randomQuote])
+    const getQuote = async () => {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        const random = data[Math.floor(Math.random() * data.length)];
+        console.log(random)
+        setRandomQuote(random);
+
+    }
+
+    return (
+        <>
+            <p className={styles.quote} id="quotation">&ldquo;{randomQuote.text}&ldquo;</p>
+            <footer>
+                <p className={styles.author} id="source">-- {randomQuote.author}</p>
+            </footer>{console.log(msg)}
+            <div className="row justify-content-center">
+                <a className="col-1" href={`http://twitter.com/share?text=${msg}&url=${rfqURL}&hashtags=RefreshQuotes`}> <i className={`fa fa-share fa-beat`}></i> </a>
+                <a className="col" onClick={getQuote}> <i className={`fa fa-refresh fa-spin `}></i> </a>
+            </div>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+        </>)
+}
+
+export default Quote
